35dcedbefd87a2f103a3032baac08cf269dd2a15
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2009-08-04 07:25:57 +0800

    added a module so that dllimport statements can be used

--- mykad/mykad/MyKad.vb
+++ mykad/mykad/MyKad.vb
@@ -1,7 +1,12 @@
-﻿Public Class MyKad
+﻿
+Option Strict On
+
+Imports System.Runtime.InteropServices
+Imports System.Text
+
+Public Class MyKad
 
     Public Sub init()
-
     End Sub
 
     Public Sub cleanup()
---
+++ mykad/mykad/jpn.vb
@@ -0,0 +1,7 @@
+﻿Option Strict On
+
+Imports System.Runtime.InteropServices
+Imports System.Text
+
+Module jpn
+End Module
